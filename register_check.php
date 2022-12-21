340a41339dc30fbef416ccd38fa39aec77dad96b
iambjlu <iambjlu@gmail.com>
2022-12-21 18:51:37 +0800

    Commit Message

--- register_check.php
+++ register_check.php
@@ -42,7 +42,7 @@
         </div>
         <div class="mdl-card__actions mdl-card--border">
             <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-               href="buy.php" target="imain">
+               href="login.php" target="imain">
                 好
             </a>
         </div>
@@ -51,26 +51,32 @@
 
 <?php
 
+if ($_POST["account"] == "" || $_POST["password"] == "" || $_POST["name"] == "" || $_POST["sex"] == "" || $_POST["phone"] == "" || $_POST["address"] == "") {
+    echo '<script>
+document.getElementById("card_message").innerHTML = "註冊失敗<br>請完成會員資料填寫";
+dialog.showModal();</script>';
 
-$account = $_POST["account"];
-$password = $_POST["password"];
-$name = $_POST["name"];
-$sex = $_POST["sex"];
-$sex_zhtw="";
-if($sex=="m"){
-    $sex_zhtw="先生";
-}else{
-    $sex_zhtw="小姐";
-}
+} else {
 
-$phone = $_POST["phone"];
-$address = $_POST["address"];
+    $account = $_POST["account"];
+    $password = $_POST["password"];
+    $name = $_POST["name"];
+    $sex = $_POST["sex"];
+    $sex_zhtw = "";
+    if ($sex == "m") {
+        $sex_zhtw = "先生";
+    } else {
+        $sex_zhtw = "小姐";
+    }
 
-$host = 'localhost';
-$dbuser = 'root';
-$dbpassword = '';
-$dbname = 'papaya';
-$link = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
+    $phone = $_POST["phone"];
+    $address = $_POST["address"];
+
+    $host = 'localhost';
+    $dbuser = 'root';
+    $dbpassword = '';
+    $dbname = 'papaya';
+    $link = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
 
 //if($link){
 //    mysqli_query($link,'SET NAMES uff8');
@@ -80,10 +86,10 @@ $link = mysqli_connect($host, $dbuser, $dbpassword, $dbname);
 //    echo "不正確連接資料庫</br>" . mysqli_connect_error();
 //}
 
-$sql = "INSERT INTO user_data (account, password, name, sex,
+    $sql = "INSERT INTO user_data (account, password, name, sex,
     phone, address) VALUES ('$account','$password','$name' ,'$sex','$phone','$address')";
 
-$result = mysqli_query($link, $sql);
+    $result = mysqli_query($link, $sql);
 //// 如果有異動到資料庫數量(更新資料庫)
 //if (mysqli_affected_rows($link)>0) {
 //// 如果有一筆以上代表有更新
@@ -97,12 +103,12 @@ $result = mysqli_query($link, $sql);
 //else {
 //    echo "{$sql} 語法執行失敗，錯誤訊息: " . mysqli_error($link);
 //}
-
+}
 ?>
 
 <script>
     let message = "<?php
-        echo $name . $sex_zhtw . "您好!<br>恭喜您已註冊完成囉!";
+        echo $name . $sex_zhtw . "您好!<br>恭喜您已註冊完成囉!<br>立刻前往登入吧!";
         ?>";
     document.getElementById("card_message").innerHTML = message;
 </script>
