accebe6f320c581cec36bf48c7dce3fc1691262d
5a8g0005 <5a8g0005@stust.edu.tw>
2022-12-27 12:41:49 +0800

    Commit Message

--- line_bot.php
+++ line_bot.php
@@ -23,13 +23,13 @@ mysqli_close($link);
 
 //採用config.ini方案的話
 //if (file_exists(__DIR__ . '/config.ini')) {
-//    $config = parse_ini_file("config.ini", true); //解析配置檔
-//    if ($config['Channel']['Token'] == null || $config['Channel']['Secret'] == null) {
-//        error_log("config.ini 配置檔未設定完全！", 0); //輸出錯誤
-//    } else {
-//        $channelAccessToken = $config['Channel']['Token'];
-//        $channelSecret = $config['Channel']['Secret'];
-//    }
+$config = parse_ini_file("config.ini", true); //解析配置檔
+if ($config['Channel']['Token'] == null || $config['Channel']['Secret'] == null) {
+    error_log("config.ini 配置檔未設定完全！", 0); //輸出錯誤
+} else {
+    $channelAccessToken = $config['Channel']['Token'];
+    $channelSecret = $config['Channel']['Secret'];
+}
 //} else {
 //    $configFile = fopen("config.ini", "w") or die("Unable to open file!");
 //    $configFileContent = '
@@ -45,42 +45,128 @@ mysqli_close($link);
 //    fclose($configFile); //關閉文件
 ////    error_log("config.ini 配置檔建立成功，請編輯檔案填入資料！", 0); //輸出錯誤
 //}
-
 $message = null;
 $event = null;
-
+$status = "none";
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message': //訊息觸發
             $message = $event['message'];
             switch ($message['type']) {
-                case 'text': //訊息為文字
-                    require_once('includes/text.php'); //Type: Text
-                    require_once('includes/image.php'); //Type: Image
-                    require_once('includes/video.php'); //Type: Video
-                    require_once('includes/audio.php'); //Type: Audio
-                    require_once('includes/location.php'); //Type: Location
-                    require_once('includes/sticker.php'); //Type: Sticker
-                    require_once('includes/imagemap.php'); //Type: Imagemap
-                    require_once('includes/template.php'); //Type: Template
-                    require_once('includes/flex.php'); //Type: Flex
+                case 'text':
+// //訊息為文字
+//                    require_once('includes/text.php'); //Type: Text
+//                    require_once('includes/image.php'); //Type: Image
+//                    require_once('includes/video.php'); //Type: Video
+//                    require_once('includes/audio.php'); //Type: Audio
+//                    require_once('includes/location.php'); //Type: Location
+//                    require_once('includes/sticker.php'); //Type: Sticker
+//                    require_once('includes/imagemap.php'); //Type: Imagemap
+//                    require_once('includes/template.php'); //Type: Template
+                    switch ($status) {
+                        case'none':
+                            switch ($message['text']) {
 
+                                case '選單':
+                                    $client->replyMessage(array(
+                                        'replyToken' => $event['replyToken'],
+                                        'messages' => array(
+                                            array(
+                                                'type' => 'text',
+                                                'text' => '您好，歡迎使用木瓜書城官方帳號
+請輸入文字進行操作:
+1) 綁定木瓜書城帳號
+2) 近三筆登入紀錄
+3) 查詢近三筆訂單
+4) 前往書城首頁(網址可能隨時更新)
+5) 轉接Messenger真人客服
+
+輸入[選單]可隨時呼叫此選單。')
+                                        )
+                                    ));
+                                    break;
+
+                                case '1':
+                                    $status = "account";
+                                    $client->replyMessage(array(
+                                        'replyToken' => $event['replyToken'],
+                                        'messages' => array(
+                                            array(
+                                                'type' => 'text', //訊息類型 (文字)
+                                                'text' => '請輸入帳號。
+如欲取消，請輸入取消。' //回覆訊息
+                                            )
+                                        )
+                                    ));
+                                    $status = "password";
+                                    break;
+
+                                case '2':
+                                    $status = "account";
+                                    $client->replyMessage(array(
+                                        'replyToken' => $event['replyToken'],
+                                        'messages' => array(
+                                            array(
+                                                'type' => 'text', //訊息類型 (文字)
+                                                'text' => '請輸入帳號。
+如欲取消，請輸入取消。' //回覆訊息
+                                            )
+                                        )
+                                    ));
+                                    $status = "password";
+                                    break;
+
+                                default:
+                                    $client->replyMessage(array(
+                                        'replyToken' => $event['replyToken'],
+                                        'messages' => array(
+                                            array(
+                                                'type' => 'text', //訊息類型 (文字)
+                                                'text' => '哈囉。' //回覆訊息
+                                            )
+                                        )
+                                    ));
+                                    break;
+                            }
+                            break;
+
+
+                        default:
+                            //error_log("Unsupporeted message type: " . $message['type']);
+                            break;
+                    }
+                case 'account':
+                    $status = "password";
                     $client->replyMessage(array(
                         'replyToken' => $event['replyToken'],
                         'messages' => array(
                             array(
-                                'type' => 'text',
-                                'text' => '您好，這是一個範例 Bot OuO
-範例程式開源至 GitHub (包含教學)：
-https://github.com/GoneTone/line-example-bot-php'
+                                'type' => 'text', //訊息類型 (文字)
+                                'text' => '請輸入密碼。
+如欲取消，請輸入取消。' //回覆訊息
                             )
                         )
                     ));
-
+                    $status = "password";
                     break;
+
+                case 'password':
+                    $status = "password";
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text', //訊息類型 (文字)
+                                'text' => '請輸入密碼。
+如欲取消，請輸入取消。' //回覆訊息
+                            )
+                        )
+                    ));
+                    $status = "password";
+                    break;
+
                 default:
-                    //error_log("Unsupporeted message type: " . $message['type']);
                     break;
             }
             break;
